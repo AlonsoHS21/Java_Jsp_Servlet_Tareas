61c1c5069267f86f6eaaf3a1c7b877dbf699a41c
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-09 20:19:36 -0300

    Creo la clase TipoTarea con sus atributos

---
+++ src/entity/TipoTarea.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class TipoTarea {
+	
+	private String nombre;
+	
+	public TipoTarea() {
+		nombre = ""; //Vacio
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}	
