768580c2f24cc7422a6f3022bb61a691b0d8a9fa
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-09 20:31:34 -0300

    Creo la clase TareasControl en Control(Negocio)

---
+++ src/control/TareasControl.java
@@ -0,0 +1,9 @@
+package control;
+
+import java.util.ArrayList;
+
+import entity.Tarea;
+
+public interface TareasControl {
+	public ArrayList<Tarea> listaDeTareas(); //TODO: Despues va a recibir el id de usuario
+}
--- src/controlImpl/TareasControlImpl.java
+++ src/controlImpl/TareasControlImpl.java
@@ -1,13 +1,12 @@
 package controlImpl;
 
 import java.util.ArrayList;
-
-import control.Tareas;
+import control.TareasControl;
 import dao.TareasDao;
 import daoImpl.TareasDaoImpl;
 import entity.Tarea;
 
-public class TareasControlImpl implements Tareas{
+public class TareasControlImpl implements TareasControl{
 	//TODO: Aca puedo hacer algunas validaciones
 	@Override
 	public ArrayList<Tarea> listaDeTareas() {
