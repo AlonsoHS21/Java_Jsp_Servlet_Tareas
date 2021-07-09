ad5e829773f33f3e6169fb0810d07e12db021c18
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-09 20:32:03 -0300

    Creo interfaz TareaDao en Dao

---
+++ src/dao/TareasDao.java
@@ -0,0 +1,9 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entity.Tarea;
+
+public interface TareasDao {
+	public ArrayList<Tarea> listaDeTareas(); //TODO: Despues va a recibir el id de usuario
+}
