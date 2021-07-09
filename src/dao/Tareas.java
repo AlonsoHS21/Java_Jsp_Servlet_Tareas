749c9d02718eb6a66a6539f3086b003e38491821
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-09 20:21:40 -0300

    Interface Tareas

---
+++ src/dao/Tareas.java
@@ -0,0 +1,9 @@
+package dao;
+
+import java.util.ArrayList;
+
+import entity.Tarea;
+
+public interface Tareas {
+	public ArrayList<Tarea> listaDeTareas(); //TODO: Despues va a recibir el id de usuario 
+}
